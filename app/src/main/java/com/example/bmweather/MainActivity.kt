f1af5c1507a576ef385304746e339ac45579d4af
Mahyar <mahyar.zarnegar@brickmakers.de>
2020-07-06 13:31:06 +0200

    toast message function added

--- app/src/main/java/com/example/bmweather/MainActivity.kt
+++ app/src/main/java/com/example/bmweather/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.bmweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun toastMessage (){
+
+
+        Toast.makeText(this, "this is a toast message",
+            Toast.LENGTH_SHORT).show()
     }
 }
