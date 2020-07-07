7c269a3beabb7629fd7406827bc8eb1f4c45aafa
Mahyar <mahyar.zarnegar@brickmakers.de>
2020-07-07 16:54:39 +0200

    UI + Function
    textView für den Datenanzeiger
    clear on click function für Search field
    length of the search field text kann max 1 Zeile sein

--- app/src/main/java/com/example/bmweather/MainActivity.kt
+++ app/src/main/java/com/example/bmweather/MainActivity.kt
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun toastMessage (){
 
+        search_button.setOnClickListener {
+            Toast.makeText(this, "this is a toast message",
+                Toast.LENGTH_SHORT).show()
+        }
 
-        Toast.makeText(this, "this is a toast message",
-            Toast.LENGTH_SHORT).show()
+        // clears the autoCompleteTExtView when it is clicked
+        autoCompleteTextView.setOnClickListener {
+            autoCompleteTextView.setText("")
+        }
+
     }
+
 }
